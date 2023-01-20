66f433d6efffd20759f7da0a22636cfc0488a92a
Aaron <aaron@clabs.co>
2023-01-20 12:12:57 -0800

    add vercel analytics

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from 'next/app'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 import 'styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY as string}>
-            <Component {...pageProps} />
-  </GoogleReCaptchaProvider>
+  return (
+    <>
+      <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY as string}>
+        <Component {...pageProps} />
+      </GoogleReCaptchaProvider>
+      <Analytics/>
+    </>
+  )
 }
