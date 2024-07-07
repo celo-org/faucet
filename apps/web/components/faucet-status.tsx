c3ef591df1d5e2071968ad0cb1ea1dbaf15cc82d
pahor167 <hornak.pavel@gmail.com>
2024-07-07 10:52:27 +0200

    explorer fix

--- apps/web/components/faucet-status.tsx
+++ apps/web/components/faucet-status.tsx
@@ -1,5 +1,5 @@
-import { FC, useCallback, useEffect, useState } from 'react'
 import { inter } from 'components/request-form'
+import { FC, useCallback, useEffect, useState } from 'react'
 import styles from 'styles/Form.module.css'
 import { Network, RequestRecord, RequestStatus } from 'types'
 import { subscribeRequest } from 'utils/firebase-client'
@@ -95,7 +95,7 @@ const TxMessage = ({
       className={inter.className}
       target="_blank"
       rel="noreferrer"
-      href={`https://explorer.celo.org/${network}/tx/${txHash}`}
+      href={network == 'dango' ? `https://celo-dango.blockscout.com/tx/${txHash}` : `https://explorer.celo.org/${network}/tx/${txHash}`}
     >
       View on Celo Explorer
     </a>
