0fb67dd781f757e6fb9749589a77c0e3f34e7b23
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-06-20 15:02:12 +0200

    castaway

---
+++ apps/web/pages/mini.tsx
@@ -0,0 +1,215 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Inter } from 'next/font/google'
+import React, { FC, FormEvent, useCallback, useEffect, useRef, useState } from "react"
+import { useAsyncCallback } from 'react-use-async-callback'
+import { Hex, isAddress } from 'viem'
+import { celoAlfajores } from 'viem/chains'
+import {
+  useAccount,
+  useConnect,
+  usePublicClient,
+  useSignTypedData,
+  useWaitForTransactionReceipt,
+  WagmiProvider
+} from 'wagmi'
+
+import { sdk } from "@farcaster/frame-sdk"
+import { config } from "../config/wagmi"
+import farcasterStyles from '../styles/Farcaster.module.css'
+import styles from '../styles/Form.module.css'
+import {
+  EIP712Domain,
+  FaucetRequest,
+  FaucetRequest712Type,
+  FaucetResponce,
+} from '../utils/mini-faucet'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+
+
+function App() {
+  useEffect(() => {
+    // this is a promise that resolves when we are ready 
+    void sdk.actions.ready();
+  }, []);
+  return (
+    <div className={farcasterStyles.main}>
+      <h1 className={inter.className}>Celo Tap (Alpha)</h1>
+      <ConnectMenu />
+    </div>
+  );
+}
+
+function ConnectMenu() {
+  const { isConnected, address } = useAccount();
+  const { connect, connectors } = useConnect();
+
+  if (isConnected) {
+    return (
+      <>
+        <MiniFaucet />
+      </>
+    );
+  }
+
+  return (
+    <button type="button" onClick={() => connect({ connector: connectors[0] })}>
+      Connect
+    </button>
+  );
+}
+
+
+const queryClient = new QueryClient();
+
+export default function Main() {
+  return (
+     <React.StrictMode>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </React.StrictMode>
+  )
+};
+
+
+
+export const MiniFaucet: FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const { signTypedDataAsync } = useSignTypedData()
+  const account = useAccount()
+  const client = usePublicClient()
+  const [txHash, setTXhash] = useState<Hex | undefined>()
+
+  const receipt = useWaitForTransactionReceipt({ hash: txHash })
+
+
+  const [onSubmit, { isExecuting, errors }] = useAsyncCallback(
+    async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+
+      const beneficiary = inputRef.current?.value
+      console.info('begin tap sequence')
+      if (
+        !beneficiary?.length ||
+        !isAddress(beneficiary) ||
+        !account.address ||
+        !client
+      ) {
+        console.info('aborting')
+        return
+      }
+
+      const message: FaucetRequest['message'] = {
+        beneficiary,
+        verification: 'farcaster',
+        chainId: celoAlfajores.id,
+      }
+
+      const signature = await signTypedDataAsync({
+        domain: EIP712Domain(celoAlfajores.id),
+        types: FaucetRequest712Type.types,
+        primaryType: FaucetRequest712Type.primaryType,
+        message,
+      })
+
+      const request: FaucetRequest = {
+        signer: account.address,
+        signature,
+        domain: EIP712Domain(celoAlfajores.id),
+        message,
+      }
+
+      const response = await fetch('api/tap', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(request),
+      })
+      // TODO get key from result and set
+      const result = (await response.json()) as FaucetResponce
+      // TODO show the tx hash and wait for it in the UI
+      if (result.ok) {
+        setTXhash(result.hash)
+      } else {
+        throw new Error(result.message)
+      }
+    },
+    [
+      inputRef,
+      client,
+      signTypedDataAsync,
+      celoAlfajores.id,
+      account.address,
+    ],
+  )
+
+  const onInvalid = useCallback((event: FormEvent<HTMLInputElement>) => {
+    const { validity } = event.currentTarget
+    console.debug('validity input', JSON.stringify(validity))
+    if (validity.patternMismatch || validity.badInput || !validity.valid) {
+      event.currentTarget.setCustomValidity('enter an 0x address')
+    } else {
+      event.currentTarget.setCustomValidity('')
+    }
+  }, [])
+
+  const buttonDisabled =
+    isExecuting ||
+    receipt.isLoading ||
+    !account.address
+
+  return (
+    <>
+      <div className={styles.intro}>
+        {txHash && (
+          <div className={styles.success}>
+            <p className={styles.successText}>
+              Your request has been submitted! Check the transaction{' '}
+              <a
+                className={styles.txLink}
+                href={`https://alfajores.celoscan.io/tx/${txHash}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>
+            </p>
+          </div>
+        )}
+      </div>
+      <form
+        className={styles.center}
+        onSubmit={onSubmit}
+        action="api/request2"
+        method="post"
+      >
+        <label className={styles.center}>
+          <span className={styles.label}>Account Address</span>
+          <input
+            defaultValue={account.address}
+            onInvalid={onInvalid}
+            minLength={40}
+            ref={inputRef}
+            pattern="^0x[a-fA-F0-9]{40}"
+            type="text"
+            placeholder="0x01F10..."
+            className={styles.address}
+          />
+        </label>
+        <button
+          disabled={buttonDisabled}
+          className={styles.button}
+          type="submit"
+        >
+          {'Tap CELO'}
+        </button>
+      </form>
+    </>
+  )
+}
---
+++ apps/web/utils/mini-faucet.ts
@@ -0,0 +1,183 @@
+import { Redis } from '@upstash/redis'
+import {
+  Address,
+  createWalletClient,
+  extractChain,
+  Hex,
+  http,
+  parseEther,
+  SignTypedDataParameters,
+  verifyTypedData
+} from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { celoAlfajores } from 'viem/chains'
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+})
+
+// only alfajores but all thats needed to support more is for it to be added to the list and then people to request being fauceted from that domain
+const FAUCET_CHAINS = [celoAlfajores] as const
+const FAUCET_CHAIN_IDS = FAUCET_CHAINS.map(({ id }) => id)
+const determineChain = (chainId: | typeof celoAlfajores.id) =>
+  extractChain({ chains: FAUCET_CHAINS, id: chainId })
+
+async function hasBeenUsed(hash: string) {
+  console.time('hasBeenUsed')
+  const hashkey = `sig:${hash}`
+  // Key Doesn't Exist: returns 1, Key Already Exists: returns 0
+  // exists means it has been used
+  const result = await redis.setnx(hashkey, true)
+  console.timeEnd('hasBeenUsed')
+  if (result === 0) {
+    return true
+  }
+  return false
+}
+
+const PERIOD_SECONDS = 60 * 30
+
+async function getFaucetsForAccount(address: Address) {
+  console.time('getFaucetsForAccount')
+  const key = `addr:${address}`
+  const pipeline = redis.pipeline()
+  pipeline.incr(key)
+  pipeline.expire(key, PERIOD_SECONDS)
+  const result = await pipeline.exec<[number]>()
+  console.timeEnd('getFaucetsForAccount')
+  return result[0]
+}
+
+export const FaucetRequest712Type: Pick<
+  SignTypedDataParameters,
+  'primaryType' | 'types'
+> = {
+  types: {
+    FaucetRequest: [
+      { name: 'beneficiary', type: 'address' },
+      {
+        name: 'verification',
+        type: 'string',
+      },
+      {
+        name: 'blockHash',
+        type: 'string',
+      },
+    ],
+    Strategy: [
+      {
+        name: 'name',
+        type: 'string',
+      },
+    ],
+  },
+  primaryType: 'FaucetRequest',
+} as const
+
+export const EIP712Domain = (chainID: (typeof FAUCET_CHAIN_IDS)[number]) =>
+  ({
+    name: 'CeloFaucet',
+    version: '1',
+    chainId: chainID,
+  }) satisfies SignTypedDataParameters['domain']
+
+// check there is balance in the account or look at talet protocol, socialconnect/minipay, self, or we can even do a whitelist of accounts
+type SybilStrategy = 'lockcelo' | 'eth' | 'none' | 'farcaster' 
+
+export interface FaucetRequest {
+  signer: Address
+  signature: Hex
+  domain: ReturnType<typeof EIP712Domain>
+  message: {
+    beneficiary: Address
+    verification: SybilStrategy
+    chainId: typeof celoAlfajores.id
+  }
+}
+
+const MAX_FAUCETS_PER_PERIOD_PER_ACCOUNT = 30
+
+export async function verifyRequest(request: FaucetRequest) {
+
+  console.time('verifyTypedData')
+  const isAuthentic = await verifyTypedData({
+    ...FaucetRequest712Type,
+    address: request.signer,
+    signature: request.signature,
+    domain: request.domain,
+    message: request.message,
+  })
+  console.timeEnd('verifyTypedData')
+
+  ensure(isAuthentic, 'Signature is not authentic')
+
+  const [
+    alreadyUsed,
+    requestCountForAccount,
+  ] = await Promise.all([
+    hasBeenUsed(request.signature),
+    getFaucetsForAccount(request.signer),
+  ])
+ 
+  ensure(
+    requestCountForAccount <= MAX_FAUCETS_PER_PERIOD_PER_ACCOUNT,
+    'You have reached the maximum number of requests for this account in this period',
+  )
+  ensure(!alreadyUsed, 'This request has already been used')
+
+  return true
+}
+
+
+type Strategic = (params: { address: Address }) => Promise<{
+  maxValue: bigint
+  passed: boolean
+}>
+
+function ensure(predicate: boolean, message: string): void {
+  if (!predicate) {
+    throw new Error(message)
+  }
+}
+
+
+
+export async function processFaucet(request: FaucetRequest) {
+  console.time('verifyRequest')
+  const isValid = await verifyRequest(request)
+  console.timeEnd('verifyRequest')
+  if (!isValid) {
+    return false
+  }
+
+  const maxValue = parseEther('0.5') // default max value
+
+  const chain = determineChain(request.message.chainId)
+
+  const walletClient = createWalletClient({
+    chain,
+    transport: http(),
+    account: privateKeyToAccount(process.env.PRIVATE_KEY as Hex),
+  })
+
+  console.time('sendTransaction')
+  const hash = await walletClient.sendTransaction({
+    to: request.message.beneficiary,
+    chain,
+    value: maxValue,
+  })
+  console.timeEnd('sendTransaction')
+
+  return hash
+}
+
+export type FaucetResponce =
+  | {
+      hash: Hex
+      ok: true
+    }
+  | {
+      message: string
+      ok: false
+    }
